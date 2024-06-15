4e9f3463f7026a623d63099780904222cfd358d0
nikita <nikanor97@gmail.com>
2024-06-15 18:09:26 +0300

    - added user_company create in user create endpoint

--- backend/src/server/users/endpoints/create_user.py
+++ backend/src/server/users/endpoints/create_user.py
@@ -1,7 +1,11 @@
+from uuid import UUID
+
 from pydantic import BaseModel
 from sqlalchemy.exc import NoResultFound
 
 from src.db.exceptions import ResourceAlreadyExists
+from src.db.projects.db_manager.user_company import UserCompanyDbManager
+from src.db.projects.models.user_company import UserCompany
 from src.db.users.db_manager.user import UserDbManager
 from src.db.users.db_manager.user_password import UserPasswordDbManager
 from src.db.users.models.user import User, UserBase
@@ -17,6 +21,7 @@ class CreateUserRequest(BaseModel):
     is_deleted: bool
     password: str
     role: str = "user"
+    company_id: UUID
 
 
 class CreateUser(UsersEndpoints):
@@ -24,17 +29,25 @@ class CreateUser(UsersEndpoints):
         self,
         user_create: CreateUserRequest
     ) -> UnifiedResponse[User]:
-        async with self._main_db_manager.users.make_autobegin_session() as session:
-            try:
-                user = UserBase(**user_create.dict())
-                new_user = await UserDbManager.create_user(session, user)
+        async with self._main_db_manager.projects.make_autobegin_session() as projects_session:
+            async with self._main_db_manager.users.make_autobegin_session() as session:
+                try:
+                    user = UserBase(**user_create.dict())
+                    new_user = await UserDbManager.create_user(session, user)
 
-                await UserPasswordDbManager.create_user_password(
-                    session, new_user.id, user_create.password
-                )
+                    await UserPasswordDbManager.create_user_password(
+                        session, new_user.id, user_create.password
+                    )
 
-                return UnifiedResponse(data=new_user)
-            except ResourceAlreadyExists as e:
-                return UnifiedResponse(error=exc_to_str(e), status_code=409)
-            except NoResultFound as e:
-                return UnifiedResponse(error=exc_to_str(e), status_code=404)
+                    user_company = UserCompany(
+                        user_id=new_user.id, company_id=user_create.company_id
+                    )
+                    await UserCompanyDbManager.create_user_company(
+                        projects_session, user_company
+                    )
+
+                    return UnifiedResponse(data=new_user)
+                except ResourceAlreadyExists as e:
+                    return UnifiedResponse(error=exc_to_str(e), status_code=409)
+                except NoResultFound as e:
+                    return UnifiedResponse(error=exc_to_str(e), status_code=404)

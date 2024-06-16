21b38f28156e0f201f810f4cd6db847c75ab4969
Шахов Артем Сергеевич <ASShakhov@sberbank.ru>
2024-06-16 21:59:02 +0300

    fx links

--- frontend/src/pages/lk/prediction/Modules/Header/Modules/Controls/Controls.tsx
+++ frontend/src/pages/lk/prediction/Modules/Header/Modules/Controls/Controls.tsx
@@ -1,4 +1,3 @@
-import {useMemo} from 'react';
 import {Space, DatePicker, Button} from 'antd';
 import { HiOutlineDownload } from "react-icons/hi";
 import {usePrivateStore} from '../../../../Store/Store';
@@ -6,7 +5,8 @@ import styles from './Controls.module.scss';
 import { declinationOfNumber } from '@/Utils/Normalize/declinationOfNumber';
 import {useCreateRequestByPredicitions} from '@/Hooks/Requests/useCreateRequestByPredictions';
 import { useDateByQuarter } from '@/Hooks/Date/useDateByQuarter';
-
+import {Routes} from '@/Routes/Routes';
+import { useUser } from '@/Hooks/User/useUser';
 
 const Controls = () => {
     const changeParams = usePrivateStore((state) => state.actions.changeParams);
@@ -15,6 +15,8 @@ const Controls = () => {
     const params = usePrivateStore((state) => state.params);
     const createRequest = useCreateRequestByPredicitions();
     const value = useDateByQuarter(params.quarter, params.year);
+    const {data: user} = useUser();
+    if (!user) return null;
     return (
         <Space>
             {
@@ -57,11 +59,15 @@ const Controls = () => {
 
                 }}
             />
-            <Button 
-                icon={<HiOutlineDownload />}
-                size="large">
+            <a 
+                download="forecast.xlsx"
+                href={Routes.api.predictionsExportExcel(params.quarter, params.year, user.id)}>
+                <Button 
+                    icon={<HiOutlineDownload />}
+                    size="large">
                 Скачать excel
-            </Button>
+                </Button>
+            </a>
         </Space>
     )
 }
--- frontend/src/pages/lk/remains/Modules/Header/Modules/Controls/Controls.tsx
+++ frontend/src/pages/lk/remains/Modules/Header/Modules/Controls/Controls.tsx
@@ -1,17 +1,25 @@
 import {Space, Button} from 'antd';
 import { HiOutlineDownload } from "react-icons/hi";
 import { HiOutlineUpload } from "react-icons/hi";
+import Routes from '@/Routes/Routes';
+import {useUser} from '@/Hooks/User/useUser';
 
 const Controls = () => {
+    const {data: user} = useUser();
+    if (!user) return null;
     return (
         <Space 
             size={16}
             direction='horizontal'>
-            <Button 
-                icon={<HiOutlineDownload />}
-                size="large">
-                Скачать excel
-            </Button>
+            <a 
+                download="remains.xlsx"
+                href={Routes.api.remainsExportExcel(user.id)}>
+                <Button 
+                    icon={<HiOutlineDownload />}
+                    size="large">
+                    Скачать excel
+                </Button>
+            </a>
             <Button 
                 type="primary"
                 icon={<HiOutlineUpload />}

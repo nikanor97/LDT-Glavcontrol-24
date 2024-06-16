1d1af55ee71ae9f00e48f46eaea9dad6e06ebc6e
Шахов Артем Сергеевич <ASShakhov@sberbank.ru>
2024-06-16 21:38:09 +0300

    фикс цифр

--- frontend/src/pages/lk/main/Modules/Remains/Modules/Graphic/Graphic.tsx
+++ frontend/src/pages/lk/main/Modules/Remains/Modules/Graphic/Graphic.tsx
@@ -1,32 +1,42 @@
 import {ResponsiveContainer, BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar} from 'recharts';
 import {useRemains} from '../../Hooks/useRemains';
+import { getMeasurementNumber } from '@/Utils/Transform/getMeasurementNumber';
 
 const Graphic = () => {
     const {data} = useRemains();
     if (!data) return null;
     return (
         <ResponsiveContainer width="100%" height="100%">
-            <BarChart data={[
-                {
-                    name: 'Начало периода',
-                    debet: data.saldo_begin_debet,
-                    credit: data.saldo_begin_credit
-                },
-                {
-                    name: 'За период',
-                    debet: data.saldo_period_debet,
-                    credit: data.saldo_period_credit
-                },
-                {
-                    name: 'Конец периода',
-                    debet: data.saldo_end_debet,
-                    credit: data.saldo_end_credit
-                }
-            ]}>
+            <BarChart 
+                margin={{
+                    left: 40
+                }}
+                data={[
+                    {
+                        name: 'Начало периода',
+                        debet: data.saldo_begin_debet,
+                        credit: data.saldo_begin_credit
+                    },
+                    {
+                        name: 'За период',
+                        debet: data.saldo_period_debet,
+                        credit: data.saldo_period_credit
+                    },
+                    {
+                        name: 'Конец периода',
+                        debet: data.saldo_end_debet,
+                        credit: data.saldo_end_credit
+                    }
+                ]}>
                 <XAxis dataKey="name" />
                 <YAxis 
                     tickFormatter={(value) => {
-                        return value;
+                        const parsed = getMeasurementNumber(value, false);
+                        if (parsed.measurement) {
+                            return `${parsed.beautified} ${parsed.measurement}`
+                        } else {
+                            return parsed.number.toString()
+                        }
                     }}
                 />
                 <Tooltip cursor={false} />

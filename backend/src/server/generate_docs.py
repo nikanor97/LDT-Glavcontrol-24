fe436e710aee1e0b11dcd3b8c9b926f65db026b0
nikita <nikanor97@gmail.com>
2024-06-24 19:28:52 +0300

    - updated word generator

--- backend/src/server/generate_docs.py
+++ backend/src/server/generate_docs.py
@@ -24,74 +24,91 @@ def set_cell_merge(cell, axis_type):
 def generate_docx(items_list, item_properties, output_file):
     # Создаем новый документ
     doc = Document()
-    
+
     # Первая страница с центровкой текста "Техническое задание"
     section = doc.sections[0]
     section.page_height = Pt(842)  # Высота страницы А4 в пунктах (высота)
     section.page_width = Pt(595.2)  # Ширина страницы А4 в пунктах (ширина)
-    
+
     paragraph = doc.add_paragraph()
     run = paragraph.add_run('Техническое задание')
-    
+
     # Увеличение шрифта
     run.font.size = Pt(24)
-    
+
     # Центрирование текста по ширине и высоте
     paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
     section.vertical_alignment = WD_ALIGN_PARAGRAPH.CENTER
-    
+
     # Добавляем пустые параграфы для центровки по высоте
     before_paragraph = doc.add_paragraph()
     before_paragraph.add_run('').add_break()
     before_paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
-    
+
     # Центрация за счет разрывов параграфов
     for _ in range(15):
         p = doc.add_paragraph()
         p.alignment = WD_ALIGN_PARAGRAPH.CENTER
 
     doc.add_page_break()  # Разрыв страницы
-    
+
     # Вторую страницу создаем с таблицей
-    table = doc.add_table(rows=1, cols=4)
+    table = doc.add_table(rows=1, cols=7)
     table.style = 'Table Grid'
-    
+
     # Заполняем заголовки таблицы
     hdr_cells = table.rows[0].cells
     hdr_cells[0].text = '№ п/п'
     hdr_cells[1].text = 'Наименование товара'
     hdr_cells[2].text = 'Наименование показателя'
     hdr_cells[3].text = 'Описание, значение'
-    
+    hdr_cells[4].text = 'Цена за единицу, руб.'
+    hdr_cells[5].text = 'Количество'
+    hdr_cells[6].text = 'Цена за все, руб.'
+
     # Заполняем таблицу данными из списка и словаря
     for i, item in enumerate(items_list, start=1):
         name = item.get("name", "")
+        price = item.get("price", "")
+        amount = item.get("amount", "")
         if name in item_properties:
             properties = item_properties[name].split(';')
             num_rows = len(properties)
-            
+
             # Первая строка с объединением
             first_row_cells = table.add_row().cells
             first_row_cells[0].text = str(i)
             first_row_cells[1].text = name
             first_row_cells[2].text = properties[0]
             first_row_cells[3].text = ''
-            
+            first_row_cells[4].text = str(int(price))
+            first_row_cells[5].text = str(int(amount))
+            first_row_cells[6].text = str(int(price*amount))
+
             set_cell_merge(first_row_cells[0], 'start')
             set_cell_merge(first_row_cells[1], 'start')
+            set_cell_merge(first_row_cells[4], 'start')
+            set_cell_merge(first_row_cells[5], 'start')
+            set_cell_merge(first_row_cells[6], 'start')
             # set_cell_merge(first_row_cells[3], 'start')
-            
+
             for prop in properties[1:]:
                 row_cells = table.add_row().cells
                 row_cells[0].text = ''
                 row_cells[1].text = ''
                 row_cells[2].text = prop
                 row_cells[3].text = ''
-                
+                row_cells[4].text = ''
+                row_cells[5].text = ''
+                row_cells[6].text = ''
+
                 set_cell_merge(row_cells[0], 'continue')
                 set_cell_merge(row_cells[1], 'continue')
+                set_cell_merge(row_cells[4], 'continue')
+                set_cell_merge(row_cells[5], 'continue')
+                set_cell_merge(row_cells[6], 'continue')
                 # set_cell_merge(row_cells[3], 'continue')
-    
+
     # Сохраняем документ
     doc.save(output_file)
 
